9ff69436e31cf8dbd6cfcf70af7ab9d667e70f5b
unknown <PT805@CSLAB129.CSSERVER.local>
2017-08-05 19:18:41 +0800

    amam

---
+++ MvcApplication1/Controllers/homeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class homeController : Controller
+    {
+        //
+        // GET: /home/
+
+        public ActionResult Index()
+        {
+            Infolist i = new Infolist();
+
+
+            return View(i.Accounts);
+
+
+        }
+
+        public ActionResult addv()
+        { 
+        return View();
+        
+        }
+        public ActionResult save(Account acc) 
+        {
+
+            return View();
+        
+        }
+
+    }
+}
---
+++ MvcApplication1/Models/Infolist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Infolist
+    {
+       public List<Account> Accounts;
+
+        public Infolist()
+        { 
+
+         Accounts = new List<Account>();
+
+         Accounts.Add(new Account { ID = 1,name = "My name1",age = 12});
+         Accounts.Add(new Account { ID = 2, name = "My name2", age = 13 });
+         Accounts.Add(new Account { ID = 3, name = "My name3", age = 14 });
+        }
+    }
+}
